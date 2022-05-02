2e5f286c5c65d79c02826c81984cac64bd303c78
Hoseon <dawntawnclock@gmail.com>
2022-05-03 00:08:29 +0900

    응애

---
+++ JS 강의/tab.js
@@ -0,0 +1,26 @@
+$(".tab-button")
+  .eq(0)
+  .on("click", function () {
+    $(".tab-button").removeClass("orange");
+    $(".tab-button").eq(0).addClass("orange");
+    $(".tab-content").removeClass("show");
+    $(".tab-content").eq(0).addClass("show");
+  });
+$(".tab-button")
+  .eq(1)
+  .on("click", function () {
+    $(".tab-button").removeClass("orange");
+    $(".tab-button").eq(1).addClass("orange");
+    $(".tab-content").removeClass("show");
+    $(".tab-content").eq(1).addClass("show");
+  });
+$(".tab-button")
+  .eq(2)
+  .on("click", function () {
+    $(".tab-button").removeClass("orange");
+    $(".tab-button").eq(2).addClass("orange");
+    $(".tab-content").removeClass("show");
+    $(".tab-content").eq(2).addClass("show");
+  });
+
+$(".123").addClass;
