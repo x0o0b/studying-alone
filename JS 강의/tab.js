dc9400fc629c874c4be9a84c9b06432fec306953
Hoseon <dawntawnclock@gmail.com>
2022-05-06 10:41:45 +0900

    어제 깜빡하고 커밋을 안했네

--- JS 강의/tab.js
+++ JS 강의/tab.js
@@ -1,26 +1,10 @@
-$(".tab-button")
-  .eq(0)
-  .on("click", function () {
-    $(".tab-button").removeClass("orange");
-    $(".tab-button").eq(0).addClass("orange");
-    $(".tab-content").removeClass("show");
-    $(".tab-content").eq(0).addClass("show");
-  });
-$(".tab-button")
-  .eq(1)
-  .on("click", function () {
-    $(".tab-button").removeClass("orange");
-    $(".tab-button").eq(1).addClass("orange");
-    $(".tab-content").removeClass("show");
-    $(".tab-content").eq(1).addClass("show");
-  });
-$(".tab-button")
-  .eq(2)
-  .on("click", function () {
-    $(".tab-button").removeClass("orange");
-    $(".tab-button").eq(2).addClass("orange");
-    $(".tab-content").removeClass("show");
-    $(".tab-content").eq(2).addClass("show");
-  });
-
-$(".123").addClass;
+for (let i = 0; i < $(".tab-button").length; i++) {
+  $(".tab-button")
+    .eq(i)
+    .on("click", function () {
+      $(".tab-button").removeClass("orange");
+      $(".tab-button").eq(i).addClass("orange");
+      $(".tab-content").removeClass("show");
+      $(".tab-content").eq(i).addClass("show");
+    });
+}
