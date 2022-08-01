0f191019e197ee16a0c07bbeb587c7660fcd12db
Hoseon <dawntawnclock@gmail.com>
2022-08-02 01:38:03 +0900

    응애

--- shop/src/component/Detail.js
+++ shop/src/component/Detail.js
@@ -1,5 +1,5 @@
 /*eslint-disable*/
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components'
 
@@ -10,11 +10,30 @@ let YellowBtn = styled.button`
 `
 
 function Detail(props) {
+
+  useEffect(()=>{
+    console.log('안녕')
+  });
+
+  
+  let [alert, setAlert] = useState(false);
+  let [count, setCount] = useState(0);
   let {id} = useParams();
   let findItem = props.shoes.find((x)=> x.id ==id );
+  setTimeout(()=>{{setAlert(true)}}, 2000)
+  
 
     return (
         <div className="container">
+          {
+            alert == false ? 
+              <div className='alert alert-warning' style={{margin:'10px'}}>
+                2초이내 구매시 할인
+              </div> 
+            : null
+          }
+          {count}
+          <button onClick={()=>{setCount(count + 1);}}>버튼</button>
           <div className="row">
             <div className="col-md-6">
               <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
@@ -29,5 +48,6 @@ function Detail(props) {
         </div> 
     );
 }
+
 
 export default Detail;
--- shop/src/component/List.js
+++ shop/src/component/List.js
@@ -1,10 +1,12 @@
 /*eslint-disable*/
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function List(props) {
+    let navigate = useNavigate()
     return (       
         <div className="col-md-4">
-            <img src={ props.shoes.img } width="80%"/>
+            <img src={ props.shoes.img } width="80%" onClick={()=>{navigate('/detail/'+props.shoes.id)}}/>
             <h4>{ props.shoes.title }</h4>
             <p>{ props.shoes.price }</p>
         </div> 
