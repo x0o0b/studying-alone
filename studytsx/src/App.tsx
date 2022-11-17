c05062e5be8ba1a4adf39e9102c343139306c1bf
Hoseon <dawntawnclock@gmail.com>
2022-11-18 01:32:48 +0900

    응애

--- studytsx/src/App.tsx
+++ studytsx/src/App.tsx
@@ -8,12 +8,12 @@ function App() {
   return (
     <div>
       <h4>안녕하세요</h4>
-      <Profile name="철수" age="20"></Profile>
+      <Profile name="철수" age='20'></Profile>
     </div>
   );
 }
 
-function Profile(props :{name:string, age:number}) :JSX.Element {
+function Profile(props :{name:string, age:string}) :JSX.Element {
   return (
     <div>{props.name}입니다</div>
   )
