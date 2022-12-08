157e5d187415b308929734c612a0729c8f9c9b30
Hoseon <dawntawnclock@gmail.com>
2022-12-08 23:58:52 +0900

    start nextjs

---
+++ nextjs-intro/pages/index.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import NavBar from "../components/NavBar";
+
+export default function Home() {
+  return (
+    <div>
+      <NavBar />
+      <h1>h2루</h1>
+    </div>
+  );
+}
