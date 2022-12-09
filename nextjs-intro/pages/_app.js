4050587defac8ee88bf6aef263056b38dc0a03cc
sunminwoo <sgagacavg@gmail.com>
2022-12-09 15:02:01 +0900

    Custom App

---
+++ nextjs-intro/pages/_app.js
@@ -0,0 +1,13 @@
+import NavBar from "../components/NavBar";
+import "../styles/globals.css";
+
+export default function App({Component, pageProps}) {
+    return (
+        <>
+            <NavBar/>
+            <Component {...pageProps} />
+            
+        </>
+    );
+    
+}
--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,10 +1,8 @@
 import { useState } from "react";
-import NavBar from "../components/NavBar";
 
 export default function Home() {
   return (
     <div>
-      <NavBar />
       <h1>h2루</h1>
     </div>
   );
