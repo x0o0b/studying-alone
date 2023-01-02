852ffa523cabb42e7dd4833836b8183d2207ae4c
Hoseon <dawntawnclock@gmail.com>
2023-01-03 01:27:10 +0900

    Variables

--- learn-dart/main.dart
+++ learn-dart/main.dart
@@ -1,3 +1,20 @@
 void main(List<String> args) {
-  print('hello world');
+  /// #1.0 Hello World
+  print("hello world");
+
+  /// #1.1 The Var Keyword
+  var name = "tokoyami"; // Method 1
+  String name1 = "towa"; // Method 2
+  name = "tokoyami towa";
+
+  /// #1.2 Dynamic Type
+  dynamic Vtube; // dynamic type
+  Vtube = 'towa';
+  Vtube = 12345;
+  Vtube = true;
+  var Zzz; // dynamic type
+
+  /// #1.3 Nullable Variables
+  String? Brain = "useless"; // String or null
+  Brain = null;
 }
